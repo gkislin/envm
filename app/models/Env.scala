d55c407d91bcdaaf173a3100409ab9694d78fa96
Grigory Kislin <gkislin@yandex.ru>
2013-07-29 12:37:04 +0400

    Add vpn

--- app/models/Env.scala
+++ app/models/Env.scala
@@ -26,22 +26,24 @@ object Env {
 
   def vhost(serverName: String, name: String): VHost = server(serverName).vhosts.find(_.name == name).get
 
-  def vhost(hostName: String): VHost = env.servers.map(_.vhosts).flatten.find(_.name==hostName).get
-//  ; vhost<-server.vhosts; if vhost.name==hostName) yield vhost).head
+  def vhost(hostName: String): VHost = env.servers.map(_.vhosts).flatten.find(_.name == hostName).get
+  //  ; vhost<-server.vhosts; if vhost.name==hostName) yield vhost).head
 
   def serverHosts(serverName: String): Seq[ExtVHost] = extVHosts.filter(_.serverName == serverName)
 
   def parse(): Env = Json.parse(loadEnv).as[Env]
 
-  def url(protocol: String, port: port, path: String): String = s"$protocol://${env.accessIp}:$port/$path"
+  def url(protocol: String, ip:String, port: port, path: String): String = s"$protocol://$ip:$port/$path"
 
-  def ssh(host: Host, ip: String = env.accessIp) = s"ssh -p ${host.ssh} ${host.user}@$ip"
+  def vpnSsh(server: Server) = s"ssh ${server.userValue}@${server.vpnIp.get}"
+  def ssh(server: Server) = s"ssh -p ${server.ssh} ${server.userValue}@${server.ip}"
+  def ssh(host: VHost) = s"ssh -p ${host.sshPort} ${host.userValue}@${host.accessIp}"
 
-  def psql(vhost: VHost, ip: String = env.accessIp) = s"jdbc:postgresql://$ip:${vhost.psql}/rdb"
+  def psql(vhost: VHost) = s"jdbc:postgresql://${vhost.accessIp}:${vhost.psqlPort}/rdb"
 
-  def psqlExt(vhost: VHost, ip: String = env.accessIp) = {
+  def psqlExt(vhost: VHost) = {
     val (user, passw) = getLoginPassw("psql", Some(vhost))
-    s"jdbc:postgresql://$ip:${vhost.psql}/rdb?user=$user&password=$passw"
+    s"jdbc:postgresql://${vhost.accessIp}:${vhost.psqlPort}/rdb?user=$user&password=$passw"
   }
 
   def reload() {
@@ -56,21 +58,48 @@ object Env {
 trait Host {
   val name: String
   val descr: String
-  val ssh: Env.port
+  val user: Option[String]
   val ip: String
-  val user: String
+  val vpnIp: Option[String]
+
+  lazy val userValue = user.getOrElse(Env.env.user)
 }
 
-case class Env(accessIp: String, servers: List[Server])
+case class Env(tomcat: Env.port,
+               wso2: Env.port,
+               wsdl: Env.port,
+               psql: Env.port,
+               ssh: Env.port,
+               ctx: String,
+               user: String,
+               accessIp: String,
+               servers: List[Server])
 
-case class Server(name: String, descr: String, ssh: Env.port, ip: String, user: String,
-                  vhosts: List[VHost]) extends Host
+case class Server(name: String, descr: String, user: Option[String],
+                  ssh: Env.port,
+                  ip: String,
+                  vpnIp: Option[String],
+                  vhosts: List[VHost]) extends Host {
+}
 
 case class VHost(name: String, descr: String,
-                 tomcat: Env.port,
-                 wso2: Option[Env.port], wsdl: Option[Env.port],
-                 psql: Env.port, ssh: Env.port,
-                 ip: String, ctx: String, user: String) extends Host
+                 tomcat: Option[Env.port],
+                 wso2: Option[Env.port],
+                 wsdl: Option[Env.port],
+                 psql: Option[Env.port],
+                 ssh: Option[Env.port],
+                 ip: String,
+                 vpnIp: Option[String],
+                 ctx: Option[String], user: Option[String]) extends Host {
+
+  lazy val sshPort = ssh.getOrElse(Env.env.ssh)
+  lazy val accessIp = vpnIp.getOrElse(Env.env.accessIp)
+  lazy val ctxValue = ctx.getOrElse(Env.env.ctx)
+  lazy val psqlPort = psql.getOrElse(Env.env.psql)
+  lazy val tomcatPort = tomcat.getOrElse(Env.env.tomcat)
+  lazy val wsdlPort = wsdl.getOrElse(Env.env.wsdl)
+  lazy val wso2Port = wso2.getOrElse(Env.env.wso2)
+}
 
 case class ExtVHost(name: String, descr: String,
                     var version: String, serverName: String)
